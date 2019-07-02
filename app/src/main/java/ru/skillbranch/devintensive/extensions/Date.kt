43f0a29b4f56e482d1d16e60ea9d6d13cd0c6f2c
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-02 02:51:32 -0400

    hometask_2 выполнено

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,122 @@
+package ru.skillbranch.devintensive.extensions
+
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+
+const val SECOND = 1000L
+const val MINUTE = 60* SECOND
+const val HOUR = 60* MINUTE
+const val DAY = 24* HOUR
+
+fun Date.format(pattern:String = "HH:mm:ss dd.MM.yy"): String{
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date{
+
+    var time = this.time
+
+    time += when(units){
+               TimeUnits.SECOND -> value* SECOND
+               TimeUnits.MINUTE -> value* MINUTE
+               TimeUnits.HOUR -> value* HOUR
+               TimeUnits.DAY -> value* DAY
+    }
+
+    this.time = time
+
+    return this
+
+}
+
+fun Date.humanizeDiff(): String {
+
+    var startPhrase: String = ""
+    var endPhrase: String = ""
+
+    var different: Long = Date().time
+
+    different -= when{
+        this.time>Date().time -> this.time + 1000
+        else -> this.time
+    }
+
+    var elapsedDays = different / DAY
+    different = different % DAY
+    var elapsedHours = different / HOUR
+    different = different % HOUR
+    var elapsedMinutes = different / MINUTE
+    different = different % MINUTE
+    var elapsedSeconds = different / SECOND
+
+    if(elapsedDays<0 || elapsedHours < 0 || elapsedMinutes < 0 || elapsedSeconds < 0){
+        startPhrase = "через "
+        endPhrase = ""
+
+        //>360д "более года назад"
+        when {
+            elapsedDays < -360 -> return "более чем через год"
+        }
+
+        elapsedDays = elapsedDays*-1
+        elapsedHours = elapsedHours*-1
+        elapsedMinutes = elapsedMinutes*-1
+        elapsedSeconds = elapsedSeconds*-1
+    }else{
+        //>360д "более года назад"
+        when {
+            elapsedDays > 360 -> return "более года назад"
+        }
+        startPhrase = ""
+        endPhrase = " назад"
+    }
+
+    //26ч - 360д "N дней назад"
+    when(elapsedDays){
+        in 1..360 -> return "$startPhrase$elapsedDays ${Utils.inclination(elapsedDays.toInt(),"день","дня","дней")}$endPhrase"
+    }
+    //22ч - 26ч "день назад"
+    when(elapsedHours){
+        in 22..24 -> return "${startPhrase}день$endPhrase"
+    }
+    //75мин 22ч "N часов назад"
+    when(elapsedHours){
+        in 1..22 -> return "$startPhrase$elapsedHours ${Utils.inclination(elapsedHours.toInt(),"час","часа","часов")}$endPhrase"
+    }
+    //45мин - 75мин "час назад"
+    when(elapsedMinutes){
+        in 45..60 -> return "${startPhrase}час$endPhrase"
+    }
+    //75с - 45мин "N минут назад"
+    when(elapsedMinutes){
+        in 1..60 -> return "$startPhrase$elapsedMinutes ${Utils.inclination(elapsedMinutes.toInt(),"минута","минуты","минут")}$endPhrase"
+    }
+    //45с - 75с "минуту назад"
+    when(elapsedSeconds){
+        in 45..60 -> return "${startPhrase}минуту$endPhrase"
+    }
+    //1с - 45с "несколько секунд назад"
+    when(elapsedSeconds){
+        in 2..60 -> return "$startPhrase$elapsedSeconds ${Utils.inclination(elapsedSeconds.toInt(),"секунда","секунды","секунд")}$endPhrase"
+    }
+    //0с - 1с "только что"
+    when{
+        elapsedSeconds <= 1 -> return "только что"
+    }
+
+    return ""
+
+}
+
+enum class TimeUnits{
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,79 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+
+    fun parseFullName(fullName : String? ) : Pair<String?,String?>{
+        val parts : List<String>? = fullName?.split(" ")
+        var firstName : String? = parts?.getOrNull(0)
+        firstName = when(firstName){
+            "","null",null -> null
+            else -> firstName
+        }
+        var lastName : String? = parts?.getOrNull(1)
+        lastName = when(lastName){
+            "","null",null -> null
+            else -> lastName
+        }
+
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, devider: String = " "): String {
+        val arrayRu: Array<String> = arrayOf("а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я")
+        val arrayEng: Array<String> = arrayOf("a","b","v","g","d","e","e","zh","z","i","i","k","l","m","n","o","p","r","s","t","u","f","h","c","ch","sh","sh'","","i","","e","yu","ya")
+        val parts : List<String>? = payload?.split(" ")
+        var transliterationString: String = ""
+        var symbolEng: String = ""
+        var i: Int = 0
+
+        parts?.forEach {
+
+            part -> for(symbol in part) when {
+                arrayRu.indexOf(symbol.toLowerCase().toString())==-1 -> transliterationString += symbol
+                //если символ в верхнем регитсре, то переводим в нижний и ищем в массиве киррилических символов
+                symbol.isUpperCase() -> {
+                    symbolEng = arrayEng[arrayRu.indexOf(symbol.toLowerCase().toString())]
+
+                    if(symbolEng.length>1){
+                        symbolEng = (symbolEng[0].toString().toUpperCase() + symbolEng[1].toString())
+                    }else{
+                        symbolEng = symbolEng.toUpperCase()
+                    }
+
+                    transliterationString += symbolEng
+                }
+                else -> transliterationString += arrayEng[arrayRu.indexOf(symbol.toString())]
+            }
+            i++
+            if(i<parts.size) {
+                transliterationString = transliterationString + devider
+            }
+            //если не находим символ в массиве кирилличесикх символов, то оставляем как есть это латиница
+        }
+
+        return  transliterationString
+
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+
+        return when {
+            firstName.isNullOrBlank() and lastName.isNullOrBlank() -> null
+            !firstName.isNullOrBlank() and lastName.isNullOrBlank() -> firstName?.get(0)?.toUpperCase().toString()
+            firstName.isNullOrBlank() and !lastName.isNullOrBlank() -> lastName?.get(0)?.toUpperCase().toString()
+            !firstName.isNullOrBlank() and !lastName.isNullOrBlank() -> firstName?.get(0)?.toUpperCase().toString() + lastName?.get(0)?.toUpperCase().toString()
+            else -> null
+        }
+    }
+
+    fun inclination(n: Int, form1:String, form2: String, form3: String) : String
+    {
+        val n: Int = Math.abs(n) % 100
+        val n1: Int = n % 10
+        if (n in 11..19) return form3
+        if (n1 in 2..4) return form2
+        if (n1 == 1) return form1
+        return form3
+    }
+}
+
