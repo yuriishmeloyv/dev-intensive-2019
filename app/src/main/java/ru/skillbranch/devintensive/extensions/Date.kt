802c03a3c43c5b8268459647ac50b73bbd231038
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-02 13:57:53 -0400

    hometask_2 Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,12 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
-
-import ru.skillbranch.devintensive.utils.Utils
 import java.text.SimpleDateFormat
 import java.util.*
-
-
-
+import kotlin.math.abs
+import kotlin.math.pow
 
 const val SECOND = 1000L
 const val MINUTE = 60* SECOND
@@ -35,98 +32,71 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date{
 
 }
 
-fun Date.humanizeDiff(): String {
-
-    var startPhrase: String = ""
-    var endPhrase: String = ""
-
-    var different: Long = Date().time
-
-    different -= when{
-        this.time>Date().time -> this.time + 1000
-        else -> this.time
-    }
-
-    var elapsedDays = different / DAY
-    different = different % DAY
-    var elapsedHours = different / HOUR
-    different = different % HOUR
-    var elapsedMinutes = different / MINUTE
-    different = different % MINUTE
-    var elapsedSeconds = different / SECOND
-
-    if(elapsedDays<0 || elapsedHours < 0 || elapsedMinutes < 0 || elapsedSeconds < 0){
-        startPhrase = "через "
-        endPhrase = ""
-
-        //>360д "более года назад"
-        when {
-            elapsedDays < -360 -> return "более чем через год"
-        }
-
-        elapsedDays = elapsedDays*-1
-        elapsedHours = elapsedHours*-1
-        elapsedMinutes = elapsedMinutes*-1
-        elapsedSeconds = elapsedSeconds*-1
-    }else{
-        //>360д "более года назад"
-        when {
-            elapsedDays > 360 -> return "более года назад"
-        }
-        startPhrase = ""
-        endPhrase = " назад"
-    }
-
-    //26ч - 360д "N дней назад"
-    when(elapsedDays){
-        in 1..360 -> return "$startPhrase$elapsedDays ${inclination(elapsedDays.toInt(),"день","дня","дней")}$endPhrase"
-    }
-    //22ч - 26ч "день назад"
-    when(elapsedHours){
-        in 22..24 -> return "${startPhrase}день$endPhrase"
-    }
-    //75мин 22ч "N часов назад"
-    when(elapsedHours){
-        in 1..22 -> return "$startPhrase$elapsedHours ${inclination(elapsedHours.toInt(),"час","часа","часов")}$endPhrase"
-    }
-    //45мин - 75мин "час назад"
-    when(elapsedMinutes){
-        in 45..60 -> return "${startPhrase}час$endPhrase"
-    }
-    //75с - 45мин "N минут назад"
-    when(elapsedMinutes){
-        in 1..60 -> return "$startPhrase$elapsedMinutes ${inclination(elapsedMinutes.toInt(),"минута","минуты","минут")}$endPhrase"
-    }
-    //45с - 75с "минуту назад"
-    when(elapsedSeconds){
-        in 45..60 -> return "${startPhrase}минуту$endPhrase"
-    }
-    //1с - 45с "несколько секунд назад"
-    when(elapsedSeconds){
-        in 2..60 -> return "$startPhrase$elapsedSeconds ${inclination(elapsedSeconds.toInt(),"секунда","секунды","секунд")}$endPhrase"
-    }
-    //0с - 1с "только что"
-    when{
-        elapsedSeconds <= 1 -> return "только что"
-    }
-
-    return ""
-
-}
-
-fun inclination(n: Int, form1:String, form2: String, form3: String) : String
-{
-    val n: Int = Math.abs(n) % 100
-    val n1: Int = n % 10
-    if (n in 11..19) return form3
-    if (n1 in 2..4) return form2
-    if (n1 == 1) return form1
-    return form3
-}
-
 enum class TimeUnits{
     SECOND,
     MINUTE,
     HOUR,
     DAY
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+
+    val dif = this.time -  date.time
+
+
+    var interval = when {
+        abs(dif) / SECOND.toDouble() <= 1  -> {"только что"}
+        abs(dif) / SECOND.toDouble() <= 45 -> {"несколько секунд"}
+        abs(dif) / SECOND.toDouble() <= 75 -> {"минуту назад"}
+        abs(dif) / MINUTE.toDouble() <= 45 -> {"${(abs(dif) / MINUTE).toInt()} ${getNumForm((dif / MINUTE).toInt(), TimeUnits.MINUTE)}"}
+        abs(dif) / MINUTE.toDouble() <= 75 -> {"час назад"}
+        abs(dif) / HOUR.toDouble() <= 22 -> {"${(abs(dif) / HOUR).toInt()} ${getNumForm((dif / HOUR).toInt(), TimeUnits.HOUR)}"}
+        abs(dif) / HOUR.toDouble() <= 26 -> {"день назад"}
+        abs(dif) / DAY.toDouble() <= 360 -> {"${(abs(dif) / DAY).toInt()} ${getNumForm((dif / DAY).toInt(), TimeUnits.DAY)}"}
+        else ->{""}
+    }
+
+
+    when{
+        dif / DAY.toDouble() < -360 -> interval = "более года назад"
+        dif / SECOND.toDouble() < -1  -> interval += " назад"
+        dif / SECOND.toDouble() <= 1 -> {}
+        dif / DAY.toDouble() <= 360  -> interval = "через $interval"
+        else -> interval = "более чем через год"
+    }
+
+
+    return interval
+}
+
+fun getNumForm(amount: Int, units: TimeUnits): String {
+    val posAmount = abs(amount)
+
+
+    return when(posAmount){
+        0, in 5..19 -> Plurals.MANY.get(units)
+        1 -> Plurals.ONE.get(units)
+        in 2..4 -> Plurals.FEW.get(units)
+        else -> getNumForm(reduceDigitsNum(posAmount) ,units)
+    }
+}
+
+private fun reduceDigitsNum(number: Int): Int {
+    return number % (10.0.pow((number.toString().length - 1).toDouble())).toInt()
+}
+
+enum class Plurals(private val minute: String, private val hour: String, private val day: String){
+    ONE("минуту", "час", "день"),
+    FEW("минуты", "часа", "дня"),
+    MANY("минут", "часов", "дней");
+
+
+    fun get(unit: TimeUnits): String {
+        return when(unit){
+            TimeUnits.MINUTE -> minute
+            TimeUnits.HOUR -> hour
+            TimeUnits.DAY -> day
+            else -> ""
+        }
+    }
 }
