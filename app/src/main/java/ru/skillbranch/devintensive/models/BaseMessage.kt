6adfd3a690174e670dfd59d90f04daf8dd0e91eb
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-02 12:46:50 -0400

    hometask_2 выполнено

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -13,12 +13,14 @@ abstract class BaseMessage(
 
     companion object AbstractFactory{
         var lastId = -1
-        fun makeMassage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?,  isIncoming: Boolean = false): BaseMessage{
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?,  isIncoming: Boolean = false): BaseMessage{
             lastId++
             return when(type){
                 "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
                 else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
             }
         }
+
+
     }
 }
