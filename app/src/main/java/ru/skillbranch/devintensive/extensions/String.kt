75237ea9538c253aefa9f0f67cff841ac6c27b6c
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-05 22:16:32 -0400

    hometask_2 truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -3,8 +3,8 @@ package ru.skillbranch.devintensive.extensions
 fun String.truncate(countOfSimbols: Int = 16): String {
     val str = this.trim()
     return when {
-        str.length>countOfSimbols+1 -> str.substring(0, countOfSimbols+1) + "..."
-        str.length==countOfSimbols+1 -> str.substring(0, countOfSimbols+1)
+        str.length>countOfSimbols+1 -> str.substring(0, countOfSimbols).trim() + "..."
+        str.length==countOfSimbols+1 -> str.substring(0, countOfSimbols).trim()
         else -> str
     }
 }
