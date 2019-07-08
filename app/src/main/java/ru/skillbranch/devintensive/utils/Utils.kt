8100aeb085d960ede8820958719166f3d8e91a4f
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-07 23:39:41 -0400

    hometask_2 transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -18,7 +18,7 @@ object Utils {
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, devider: String = " "): String {
+    fun transliteration2(payload: String, devider: String = " "): String {
         val arrayRu: Array<String> = arrayOf("а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я")
         val arrayEng: Array<String> = arrayOf("a","b","v","g","d","e","e","zh","z","i","i","k","l","m","n","o","p","r","s","t","u","f","h","c","ch","sh","sh'","","i","","e","yu","ya")
         val parts : List<String>? = payload?.split(" ")
@@ -53,6 +53,97 @@ object Utils {
 
         return  transliterationString
 
+    }
+
+    fun transliteration(payload: String?, divider: String = " "): String {
+
+        if(payload.isNullOrBlank()){
+            return "Необходимо передать занчение"
+        }else{
+            val translit: String = payload.replace(Regex("[абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ]")){
+
+                when(it.value){
+                    "а" -> "a"
+                    "б" -> "b"
+                    "в" -> "v"
+                    "г" -> "g"
+                    "д" -> "d"
+                    "е" -> "e"
+                    "ё" -> "e"
+                    "ж" -> "zh"
+                    "з" -> "z"
+                    "и" -> "i"
+                    "й" -> "i"
+                    "к" -> "k"
+                    "л" -> "l"
+                    "м" -> "m"
+                    "н" -> "n"
+                    "о" -> "o"
+                    "п" -> "p"
+                    "р" -> "r"
+                    "с" -> "s"
+                    "т" -> "t"
+                    "у" -> "u"
+                    "ф" -> "f"
+                    "х" -> "h"
+                    "ц" -> "c"
+                    "ч" -> "ch"
+                    "ш" -> "sh"
+                    "щ" -> "sh'"
+                    "ъ" -> ""
+                    "ы" -> "i"
+                    "ь" -> ""
+                    "э" -> "e"
+                    "ю" -> "yu"
+                    "я" -> "ya"
+                    "А" -> "A"
+                    "Б" -> "B"
+                    "В" -> "V"
+                    "Г" -> "G"
+                    "Д" -> "D"
+                    "Е" -> "E"
+                    "Ё" -> "E"
+                    "Ж" -> "Zh"
+                    "З" -> "Z"
+                    "И" -> "I"
+                    "Й" -> "I"
+                    "К" -> "K"
+                    "Л" -> "L"
+                    "М" -> "M"
+                    "Н" -> "N"
+                    "О" -> "O"
+                    "П" -> "P"
+                    "Р" -> "R"
+                    "С" -> "S"
+                    "Т" -> "T"
+                    "У" -> "U"
+                    "Ф" -> "F"
+                    "Х" -> "H"
+                    "Ц" -> "C"
+                    "Ч" -> "Ch"
+                    "Ш" -> "Sh"
+                    "Щ" -> "Sh'"
+                    "Ъ" -> ""
+                    "Ы" -> "I"
+                    "Ь" -> ""
+                    "Э" -> "E"
+                    "Ю" -> "Yu"
+                    "Я" -> "Ya"
+                    else -> it.value
+                }
+            }
+
+            val massiveOfTranlit: List<String> = translit.split(" ")
+            var n: Int = 0
+            var translitToReturn: String = ""
+            while(massiveOfTranlit.size - 1 > n){
+                translitToReturn+=massiveOfTranlit.get(n) + divider
+                n++
+            }
+            translitToReturn+=massiveOfTranlit.get(n)
+
+            return translitToReturn
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
