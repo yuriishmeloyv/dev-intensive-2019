3bf95e66653b48f1d0435a974bd56d3571b6beeb
yuriishmeloyv <yurii.shmeloyv@gmail.com>
2019-07-14 00:09:01 -0400

    hometask_3 Bender main tasks

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -30,7 +30,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NORMAL(Triple(255, 255, 255)),
         WORNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        CRITICAL(Triple(255,0,0));
 
         fun nextStatus(): Status {
             if(this.ordinal < values().lastIndex){
